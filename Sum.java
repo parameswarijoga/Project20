29d8d56d0aa8d7e59f7866af26aac80c12adf120
parameswarijoga <91303817+parameswarijoga@users.noreply.github.com>
2021-12-22 12:15:46 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Sum {
+    public static void main(String args[])
+    {
+        int a=12,b=20;
+        int c=a+b;
+        System.out.println("Sum="+c);
+    }
+}
